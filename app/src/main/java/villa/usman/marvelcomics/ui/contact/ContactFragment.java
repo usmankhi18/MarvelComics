8043fbdea366b34c7a0fa51cb1cd963869bd8aa0
Muhammad Usman Khan <usman.khi18@hotmail.com>
2020-07-09 13:17:22 +0500

    Arc Navigation Drawer

--- app/src/main/java/villa/usman/marvelcomics/ui/contact/ContactFragment.java
+++ app/src/main/java/villa/usman/marvelcomics/ui/contact/ContactFragment.java
@@ -23,13 +23,6 @@ public class ContactFragment extends Fragment {
         contactViewModel =
                 ViewModelProviders.of(this).get(ContactViewModel.class);
         View root = inflater.inflate(R.layout.fragment_contact, container, false);
-        final TextView textView = root.findViewById(R.id.text_contact);
-        contactViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
